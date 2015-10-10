ffd71c8e3673f3b768e1882a65678b66920d23cc
wraeth <wraeth@wraeth.id.au>
2015-10-10 22:19:17 +1100

    Add opts to list all pkgs and find by user
    Added options to list all packages in tree currently belonging to
    proxy-maintainer herd, as well as to list all proxy-maint packages
    maintained by a specific user. Additional code improvements, cleanup and
    fixing of PEP8 violations.
    
    Note also that outputting to a file has been removed - use redirection
    to output to a file instead.
    
    Signed-off-by: wraeth <wraeth@wraeth.id.au>

--- find-proxy-users.py
+++ find-proxy-users.py
@@ -6,14 +6,13 @@ Searches metadata.xml files in Gentoo Portage tree to find proxy maintainers.
 
 import collections
 import os
-import re
 
 Details = collections.namedtuple('Details', ['desc', 'packages'])
 
 try:
-    import xml.etree.cElementTree as ET
+    import xml.etree.cElementTree as ElementTree
 except ImportError:
-    import xml.etree.ElementTree as ET
+    import xml.etree.ElementTree as ElementTree
 
 
 def main():
@@ -23,50 +22,58 @@ def main():
     parser.add_argument(
         '-p', '--portdir', help='Portage tree root', default='/usr/portage')
     parser.add_argument(
-        '-o', '--output', help='Output file', default='userlist.txt')
-    parser.add_argument(
         '-d', '--desc', help='Include maint description', action='store_true')
+    parser.add_argument(
+        '-m', '--maintainer', help='List packages for maintainer')
+    parser.add_argument(
+        '-O', '--orphans', help='List all orphaned packages', action='store_true')
     args = parser.parse_args()
 
-    u = listProxyMaintainers(args.portdir, args.desc)
+    u = list_proxy_maintainers(args.portdir, args.desc)
     addrs = list(u.keys())
     addrs.sort()
 
-    pkgcount = 0
-    fh = open(args.output, 'a')
-    for addr in addrs:
-        for pkg in u[addr].packages:
-            print(' '.join([addr, pkg, u[addr].desc]), file=fh)
-        pkgcount += len(u[addr].packages)
-    fh.close()
+    if args.orphans:
+        all_pkgs = [p for a in addrs for p in u[a].packages]
+        all_pkgs.sort()
+        [print(p) for p in all_pkgs]
+    elif args.maintainer:
+        try:
+            [print(p) for p in u[args.maintainer].packages]
+        except KeyError:
+            print("Address not found as proxy-maintainer: %r" % args.maintainer)
+    else:
+        pkgcount = 0
+        for a in addrs:
+            [print(a, p, u[a].desc) for p in u[a].packages]
+            pkgcount += len(u[a].packages)
+        print('Got %s users maintaining %s packages' % (len(addrs), pkgcount))
 
-    print('Got {!s} users maintaining {!s} packages'.format(
-        len(addrs), pkgcount))
 
-
-def listProxyMaintainers(portdir: str, add_desc: bool = False) -> dict:
+def list_proxy_maintainers(portdir: str, add_desc: bool = False) -> dict:
     """Wrapper for listing proxy maintainers, returns dict."""
     assert isinstance(portdir, str)
     userlist = {}
-    for metadata in findMetadataFiles(portdir):
-        readMetadata(metadata, userlist, add_desc)
+    for metadata in find_metadata_files(portdir):
+        read_metadata(metadata, userlist, add_desc)
     return userlist
 
 
-def findMetadataFiles(portdir: str) -> list:
+def find_metadata_files(portdir: str) -> list:
     """Searches for metadata.xml files and returns list of paths."""
     assert isinstance(portdir, str)
-    return [os.path.join(r, 'metadata.xml')
-        for r, s, f in os.walk(portdir) if 'metadata.xml' in f]
+    for root, subdirs, files in os.walk(portdir):
+        if 'metadata.xml' in files:
+            yield os.path.join(root, 'metadata.xml')
 
 
-def readMetadata(meta_file: str, maintainers: dict, add_desc: bool = False) -> None:
+def read_metadata(meta_file: str, maintainers: dict, add_desc: bool = False) -> None:
     """Reads a metadata file and saves proxy-maint to outfile."""
     assert isinstance(meta_file, str)
     assert isinstance(maintainers, dict)
     assert os.path.isfile(meta_file)
 
-    m = ET.parse(meta_file).getroot()
+    m = ElementTree.parse(meta_file).getroot()
     try:
         if 'proxy-maintainers' not in [h.text for h in m.findall('herd')]:
             return
@@ -74,14 +81,17 @@ def readMetadata(meta_file: str, maintainers: dict, add_desc: bool = False) -> N
         return
 
     for maintainer in m.findall('maintainer'):
-        package = '/'.join(meta_file.split(os.sep)[3:5])
+        package = '/'.join(meta_file.split(os.sep)[-3:-1])
         mail = maintainer.find('email').text
         # Assume the maintainer without an official '@gentoo.org' email
         # address to be the proxy user
         if 'gentoo.org' not in mail:
-            try:
-                desc = maintainer.find('description').text.replace('\n', ' ')
-            except AttributeError:
+            if add_desc:
+                try:
+                    desc = maintainer.find('description').text.replace('\n', ' ')
+                except AttributeError:
+                    desc = ''
+            else:
                 desc = ''
             try:
                 maintainers[mail].packages.append(package)
